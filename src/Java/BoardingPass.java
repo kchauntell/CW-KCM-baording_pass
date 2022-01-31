4e1b95d1f40263911110f067e0b75907dba3e986
Kendra Chauntell <kendrachauntell@outlook.com>
2022-01-31 14:43:55 -0500

    working on generating random ticket, added a method" "

--- src/Java/BoardingPass.java
+++ src/Java/BoardingPass.java
@@ -1,8 +1,18 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 import java.util.Date;
 import java.sql.Timestamp;
+import java.util.List;
+import java.util.Random;
+import java.io.File;
 
 public class BoardingPass implements Price {
-  int ticketNumber;
+  String ticketNumber;
   Date date = new Date();
   String from;
   String to;
@@ -15,6 +25,18 @@ public class BoardingPass implements Price {
     this.date = date;
     this.from = from;
     this.to = to;
+    this.ticketNumber = genTicNum();
+  }
+
+  public String genTicNum () {
+    Random num = new Random();
+    int ticketNum = num.nextInt(1, 2000);
+    return String.valueOf(ticketNum);
+  }
+
+
+  public void writeToAFile() throws IOException {
+    Files.write(Paths.get("verifyTicketNum.txt"), genTicNum().getBytes());
   }
 
 
@@ -24,16 +46,16 @@ public class BoardingPass implements Price {
         "\nDepart From: " + from +
         "\nDeparture Time: " + departureTime +
         "\nEstimated Arrival Time: " + arrivalTime +
-        "\nArriving at: " + to;
+        "\nArriving at: " + to +
+        "\nYour Ticket Number is : " + ticketNumber;
   }
 
 
   public static void main(String[] args){
+    Random ticketNumber = new Random(200);
     Date created = new Date();
     BoardingPass pass = new BoardingPass(created, "Richmond, VA", "Raleigh, NC");
     Client newClient = new Client("Kendricka", "Sonder", "ks@something.com", "9107291805", 'M', 45);
     System.out.println(pass + "\n" + pass.message() + "Your Ticket Price is " + pass.price(newClient.age, newClient.gender));
-    System.out.println(pass.message2());
-
   }
 }
--- src/Java/Price.java
+++ src/Java/Price.java
@@ -10,4 +10,6 @@ public interface Price {
 
     return ticketPrice;
   }
+
+
 }
