d8c4366078c81df84ccbab3c911b0d5816f7e34a
Kendra Chauntell <kendrachauntell@outlook.com>
2022-01-31 15:22:11 -0500

    working on adding randomNumber to file

--- src/Java/BoardingPass.java
+++ src/Java/BoardingPass.java
@@ -1,15 +1,11 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
-import java.util.Date;
+import java.util.*;
 import java.sql.Timestamp;
-import java.util.List;
-import java.util.Random;
-import java.io.File;
 
 public class BoardingPass implements Price {
   String ticketNumber;
@@ -21,22 +17,60 @@ public class BoardingPass implements Price {
   public Timestamp boardingTime;
 
 
-  BoardingPass (Date date, String from, String to) {
+  BoardingPass (Date date, String from, String to) throws IOException {
     this.date = date;
     this.from = from;
     this.to = to;
     this.ticketNumber = genTicNum();
   }
 
-  public String genTicNum () {
+  public String genTicNum () throws IOException {
+    FileWriter fw = new FileWriter("verifyTicketNum.txt", true);
+    BufferedWriter bw = new BufferedWriter(fw);
+    PrintWriter pw = new PrintWriter(bw);
+
     Random num = new Random();
     int ticketNum = num.nextInt(1, 2000);
+
+
+//    PrintWriter ticketFile = new PrintWriter(
+//        new BufferedWriter(
+//            new FileWriter("verifyTicketNum.txt")
+//        )
+//    );
+    pw.println(ticketNum);
+    pw.flush();
     return String.valueOf(ticketNum);
   }
 
+//  public class Dices {
+//    public static void main(String[] args) {
+//      Random ran = new Random();
+//      int number = 0;
+//      try (PrintWriter file = new PrintWriter(
+//          new BufferedWriter(
+//              new FileWriter("test1.txt")));
+//      ) {
+//
+//        for (int i = 1; i <= 10; i++) {
+//          number = ran.nextInt(6) + 1;
+//          file.println(number);
+//        }
+//      } catch (IOException e) {
+//        e.printStackTrace();
+//      }
+//
+//      System.out.println("File test1.txt has been created!");
+//    }
+//  }
+//  public ArrayList<String> verifiedTicketNumber() throws IOException {
+//    ArrayList<String> ticketFile = new ArrayList<String>();
+//
+//    ticketFile.addAll(Files.readAllLines(Paths.get("verifyTicketNum.txt")));
+//  }
 
   public void writeToAFile() throws IOException {
-    Files.write(Paths.get("verifyTicketNum.txt"), genTicNum().getBytes());
+    Files.write(Paths.get("verifyTicketNum.txt"), this.ticketNumber.getBytes());
   }
 
 
@@ -51,7 +85,7 @@ public class BoardingPass implements Price {
   }
 
 
-  public static void main(String[] args){
+  public static void main(String[] args) throws IOException {
     Random ticketNumber = new Random(200);
     Date created = new Date();
     BoardingPass pass = new BoardingPass(created, "Richmond, VA", "Raleigh, NC");
