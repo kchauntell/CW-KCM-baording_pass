922e1c2227bbf2df1f8247f5f60106ad53cdcab2
Kendra Chauntell <kendrachauntell@outlook.com>
2022-01-31 10:23:16 -0500

    initial commit

---
+++ src/Java/BoardingPass.java
@@ -0,0 +1,10 @@
+package Java;
+import java.util.Date;
+
+public class BoardingPass {
+  int number;
+  Date date = new Date();
+  String origin;
+
+
+}
---
+++ src/boardpassGUI.java
@@ -0,0 +1,2 @@
+public class boardpassGUI {
+}
